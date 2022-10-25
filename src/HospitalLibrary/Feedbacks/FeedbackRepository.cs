2fad3f75efb8e55dec4934ce5f7fa6694039d5b7
Dušan Janošević <dusan.janosevic123@gmail.com>
2022-10-25 11:50:30 +0200

    Added initial model, repository and service for Feedbacks

---
+++ src/HospitalLibrary/Feedbacks/FeedbackRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HospitalLibrary.Feedbacks.Interfaces;
+using HospitalLibrary.Settings;
+using HospitalLibrary.Shared.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalLibrary.Feedbacks
+{
+    public class FeedbackRepository: BaseRepository<Feedback>, IFeedbackRepository
+    {
+        public FeedbackRepository(HospitalDbContext dataContext) : base(dataContext)
+        {
+        }
+        
+        public async Task<IEnumerable<Feedback>> FindAll()
+        {
+            return await _dataContext.Feedbacks.ToListAsync();
+        }
+    }
+}
---
+++ src/HospitalLibrary/Feedbacks/FeedbackService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HospitalLibrary.Feedbacks.Interfaces;
+using HospitalLibrary.Shared.Interfaces;
+
+namespace HospitalLibrary.Feedbacks
+{
+    public class FeedbackService: IFeedbackService
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public FeedbackService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public Task<IEnumerable<Feedback>> GetAll()
+        {
+            return _unitOfWork.FeedbackRepository.GetAll();
+        }
+    }
+}
