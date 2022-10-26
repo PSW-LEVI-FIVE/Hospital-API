d5046b39bd351318aea12a0bff66c0a146603813
srdjan1210 <businessrdjan@gmail.com>
2022-10-27 00:32:14 +0200

    Added dto for creating doctors

--- src/HospitalLibrary/Doctors/Doctor.cs
+++ src/HospitalLibrary/Doctors/Doctor.cs
@@ -26,5 +26,9 @@ namespace HospitalLibrary.Doctors
         {
             SpecialtyType = specialtyType;
         }
+
+        public Doctor()
+        {
+        }
     }
 }
---
+++ src/HospitalLibrary/Doctors/Dtos/CreateDoctorDTO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HospitalLibrary.Doctors.Dtos
+{
+    public class CreateDoctorDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string  Surname { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Uid { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]        
+        public DateTime BirthDate { get; set;  }
+        [Required]
+        public string Address { get; set; }
+        [Required] 
+        public SpecialtyType SpecialtyType { get; set; }
+        
+        public Doctor MapToModel()
+        {
+            return new Doctor
+            {
+                Name = Name,
+                Email = Email,
+                Surname = Surname,
+                Uid = Uid,
+                PhoneNumber = PhoneNumber,
+                BirthDate = BirthDate,
+                Address = Address,
+                SpecialtyType = SpecialtyType
+            };
+        }
+        
+    }
+    
+    
+
+}
