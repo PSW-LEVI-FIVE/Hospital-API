51e7c5eedf86f3420927ae66bdb061cd1b6d2e9e
Kristian Krizzan <krizan.ra196.2019@uns.ac.rs>
2022-10-28 01:35:00 +0200

    added migration for publishment

---
+++ src/HospitalLibrary/Migrations/20221027232940_Feedback_Publishment.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HospitalLibrary.Migrations
+{
+    public partial class Feedback_Publishment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Publishment",
+                table: "Feedbacks",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Publishment",
+                table: "Feedbacks");
+        }
+    }
+}
