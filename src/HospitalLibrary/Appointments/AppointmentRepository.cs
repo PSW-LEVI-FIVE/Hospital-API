52168b7c5425c725ff75cd6ff0061fd49eacc0da
srdjan1210 <businessrdjan@gmail.com>
2022-10-27 21:31:17 +0200

    Added time interval service. Added queries for filtering time intervals for room and doctor by date. Prepared function for appointment validation.

--- src/HospitalLibrary/Appointments/AppointmentRepository.cs
+++ src/HospitalLibrary/Appointments/AppointmentRepository.cs
@@ -1,12 +1,34 @@
-﻿using HospitalLibrary.Shared.Repository;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HospitalLibrary.Shared.Repository;
 using HospitalLibrary.Appointments.Interfaces;
 using HospitalLibrary.Settings;
+using Microsoft.EntityFrameworkCore;
 
 namespace HospitalLibrary.Appointments
 {
-    public class AppointmentRepository:BaseRepository<Appointment>,IAppointmentRepository
+    public class AppointmentRepository:BaseRepository<Appointment>, IAppointmentRepository
     {
         public AppointmentRepository(HospitalDbContext context): base(context) {}
 
+        public async Task<IEnumerable<TimeInterval>> GetAllRoomTakenIntervalsForDate(int roomId, DateTime date)
+        {
+            return await _dataContext.Appointments
+                .Where(a => a.RoomId == roomId)
+                .Where(a => a.StartAt.Date.Equals(date.Date))
+                .Select(a => new TimeInterval(a.StartAt, a.EndAt))
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<TimeInterval>> GetAllDoctorTakenIntervalsForDate(int doctorId, DateTime date)
+        {
+            return await _dataContext.Appointments
+                .Where(a => a.DoctorId == doctorId)
+                .Where(a => a.StartAt.Date.Equals(date.Date))
+                .Select(a => new TimeInterval(a.StartAt, a.EndAt))
+                .ToListAsync();
+        }
     }
 }
--- src/HospitalLibrary/Appointments/Interfaces/IAppointmentRepository.cs
+++ src/HospitalLibrary/Appointments/Interfaces/IAppointmentRepository.cs
@@ -1,9 +1,14 @@
-﻿using HospitalLibrary.Shared.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HospitalLibrary.Shared.Interfaces;
 
 namespace HospitalLibrary.Appointments.Interfaces
 {
     public interface IAppointmentRepository: IBaseRepository<Appointment>
     {
-        
+
+        Task<IEnumerable<TimeInterval>> GetAllRoomTakenIntervalsForDate(int roomId, DateTime date);
+        Task<IEnumerable<TimeInterval>> GetAllDoctorTakenIntervalsForDate(int doctorId, DateTime date);
     }
 }
