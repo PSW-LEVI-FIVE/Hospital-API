194ec5d370948ae22d7403449ec2e6baee169521
srdjan1210 <businessrdjan@gmail.com>
2022-10-28 13:29:27 +0200

    Refactored namespaces and removed logs

--- src/HospitalAPI/Controllers/Intranet/RoomsController.cs
+++ src/HospitalAPI/Controllers/Intranet/RoomsController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using HospitalAPI.ErrorHandling.Exceptions;
 using HospitalLibrary.Rooms;
 using HospitalLibrary.Rooms.Interfaces;
 using Microsoft.AspNetCore.Mvc;
--- src/HospitalAPI/ErrorHandling/ErrorHandlingMiddleware.cs
+++ src/HospitalAPI/ErrorHandling/ErrorHandlingMiddleware.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Net;
-using HospitalAPI.ErrorHandling;
+﻿using System.Net;
 using HospitalAPI.ErrorHandling.Exceptions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 
-namespace HospitalAPI
+namespace HospitalAPI.ErrorHandling
 {
     public static class ErrorHandlingMiddleware
     {
@@ -21,29 +19,22 @@ namespace HospitalAPI
                                 int statusCode = (int)HttpStatusCode.InternalServerError;
                                 var exception = context.Features.Get<IExceptionHandlerFeature>();
 
-                                
-
-                                if (exception != null)
+                                if (exception == null) return;
+                                    
+                                if (exception.Error.GetType().IsSubclassOf(typeof(BaseException)))
                                 {
-                                    
-                                    if (exception.Error.GetType().IsSubclassOf(typeof(BaseException)))
-                                    {
-                                        BaseException baseEx = (BaseException)exception.Error;
-                                        statusCode = baseEx.StatusCode;
-                                    }
-
-                                    context.Response.ContentType = "application/json";
-                                    context.Response.StatusCode = statusCode;
-                                    StandardErrorResponse respBody = new StandardErrorResponse { Message = exception.Error.Message, StatusCode = statusCode };
-                                    
-                                    Console.WriteLine("Printing status code: " + respBody.StatusCode);
-                                    Console.WriteLine("Status code for response: " + context.Response.StatusCode);
-                                    await context.Response.WriteAsync(
-                                        respBody.ToString()
-                                    );
-                                    await context.Response.CompleteAsync();
-
+                                    BaseException baseEx = (BaseException)exception.Error;
+                                    statusCode = baseEx.StatusCode;
                                 }
+
+                                context.Response.ContentType = "application/json";
+                                context.Response.StatusCode = statusCode;
+                                
+                                StandardErrorResponse respBody = new StandardErrorResponse { Message = exception.Error.Message, StatusCode = statusCode };
+                                
+                                await context.Response.WriteAsync( respBody.ToString() );
+                                await context.Response.CompleteAsync();
+
                             }
                         );
                     }
