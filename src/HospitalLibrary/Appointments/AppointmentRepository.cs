c12b90c569e49018f711c771c7a0dd351a0ffc36
ognjens <svraka.ra181.2019@uns.ac.rs>
2022-10-28 14:40:10 +0200

    Validation logic moved to HospitalAPI and used custom exceptions

--- src/HospitalLibrary/Appointments/AppointmentRepository.cs
+++ src/HospitalLibrary/Appointments/AppointmentRepository.cs
@@ -20,6 +20,7 @@ namespace HospitalLibrary.Appointments
             return await _dataContext.Appointments
                 .Where(a => a.RoomId == roomId)
                 .Where(a => a.StartAt.Date.Equals(date.Date))
+                .Where(a => a.State.Equals(AppointmentState.PENDING))
                 .Select(a => new TimeInterval(a.StartAt, a.EndAt))
                 .ToListAsync();
         }
@@ -29,6 +30,7 @@ namespace HospitalLibrary.Appointments
             return await _dataContext.Appointments
                 .Where(a => a.DoctorId == doctorId)
                 .Where(a => a.StartAt.Date.Equals(date.Date))
+                .Where(a => a.State.Equals(AppointmentState.PENDING))
                 .Select(a => new TimeInterval(a.StartAt, a.EndAt))
                 .ToListAsync();
         }
