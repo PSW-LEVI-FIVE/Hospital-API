e33dd097f7c3f3de17c30fa46b98b807ed815b49
DavidT <terenji.ra178.2019@uns.ac.rs>
2022-10-28 21:58:56 +0200

    refactored code

--- src/HospitalLibrary/Appointments/Dtos/AppointmentCancelledDTO.cs
+++ src/HospitalLibrary/Appointments/Dtos/AppointmentCancelledDTO.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+
 namespace HospitalLibrary.Appointments.Dtos
 {
     public class AppointmentCancelledDTO
     {
-        [Required]
-        public string PatientEmail { get; set; }
-        [Required]
-        public DateTime AppointmentTime { get; set; }
+        [Required] public string PatientEmail { get; set; }
+        [Required] public DateTime AppointmentTime { get; set; }
     }
 }
