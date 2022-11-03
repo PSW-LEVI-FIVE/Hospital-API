ce85592f3708b55fd5f2dab884e14f42542aec19
Kristian Krizzan <krizan.ra196.2019@uns.ac.rs>
2022-11-03 03:09:09 +0100

    created dto for feedbacks of home page and added functions to get that dtos

---
+++ src/HospitalLibrary/Feedbacks/Dtos/PublishedFeedbackDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary.Feedbacks.Dtos
+{
+    public class PublishedFeedbackDto
+    {
+        public string Patient { get; set; }
+
+        public string FeedbackContent { get; set; }
+
+        public PublishedFeedbackDto(string patient, string feedbackContent, bool anonymous)
+        {
+            if (anonymous)
+                Patient = "Anonymous";
+            else
+                Patient = patient;
+            FeedbackContent = feedbackContent;
+        }
+    }
+}
