1d29059daa6b3aaf9e59034af862a78c70feaa04
ognjens <svraka.ra181.2019@uns.ac.rs>
2022-11-15 17:13:12 +0100

    Added annual leave service

---
+++ src/HospitalLibrary/AnnualLeaves/AnnualLeaveService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HospitalLibrary.AnnualLeaves.Interfaces;
+using HospitalLibrary.Shared.Interfaces;
+
+namespace HospitalLibrary.AnnualLeaves
+{
+    public class AnnualLeaveService : IAnnualLeaveService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AnnualLeaveService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        
+        public Task<IEnumerable<AnnualLeave>> GetAllByDoctorId(int doctorId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
