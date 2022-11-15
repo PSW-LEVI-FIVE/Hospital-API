0df9a907b4bdebbca9b5cddc94018aec0c1291f4
ognjens <svraka.ra181.2019@uns.ac.rs>
2022-11-15 17:13:12 +0100

    Added annual leave service

---
+++ src/HospitalLibrary/AnnualLeaves/Interfaces/IAnnualLeaveService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary.AnnualLeaves.Interfaces
+{
+    public interface IAnnualLeaveService
+    {
+        Task<IEnumerable<AnnualLeave>> GetAllByDoctorId(int doctorId);
+    }
+}
