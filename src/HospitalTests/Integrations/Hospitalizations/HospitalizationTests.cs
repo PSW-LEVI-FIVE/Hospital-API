1fb0ff51594fa93055cacef2e8ce9ecff3521c9e
srdjan1210 <businessrdjan@gmail.com>
2022-11-16 20:28:53 +0100

    Added integration tests for hospitalization functionality

---
+++ src/HospitalTests/Integrations/Hospitalizations/HospitalizationTests.cs
@@ -0,0 +1,68 @@
+﻿using HospitalAPI;
+using HospitalAPI.Controllers.Intranet;
+using HospitalLibrary.Hospitalizations;
+using HospitalLibrary.Hospitalizations.Dtos;
+using HospitalLibrary.Hospitalizations.Interfaces;
+using HospitalLibrary.MedicalRecords.Interfaces;
+using HospitalTests.Setup;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace HospitalTests.Integrations.Hospitalizations;
+
+public class HospitalizationTests: BaseIntegrationTest
+{
+
+    
+    public HospitalizationTests(TestDatabaseFactory<Startup> factory) : base(factory)
+    {
+    }
+
+
+    // public HospitalizationController SetupController()
+    // {
+    //     using var scope = Factory.Services.CreateScope();
+    //     var controller = new HospitalizationController(
+    //         scope.ServiceProvider.GetRequiredService<IHospitalizationService>(),
+    //         scope.ServiceProvider.GetRequiredService<IMedicalRecordService>()
+    //     );
+    //     return controller;
+    // }
+
+    [Fact]
+    public void Hospitalization_created_successfully()
+    {
+        using var scope = Factory.Services.CreateScope();
+        var controller = new HospitalizationController(
+            scope.ServiceProvider.GetRequiredService<IHospitalizationService>(),
+            scope.ServiceProvider.GetRequiredService<IMedicalRecordService>()
+        );
+        var dto = new CreateHospitalizationDTO()
+        {
+            BedId = 1,
+            PatientId = 1,
+            StartTime = DateTime.Now,
+            MedicalRecordId = 0
+        };
+        var result = ((OkObjectResult)controller.CreateHospitalization(dto)).Value as Hospitalization;
+        result.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public void Hospitalization_ended()
+    {
+        using var scope = Factory.Services.CreateScope();
+        var controller = new HospitalizationController(
+            scope.ServiceProvider.GetRequiredService<IHospitalizationService>(),
+            scope.ServiceProvider.GetRequiredService<IMedicalRecordService>()
+        );
+        var dto = new EndHospitalizationDTO(DateTime.Now);
+        var result = ((OkObjectResult)controller.EndHospitalization(dto, 10)).Value as Hospitalization;
+        
+        result.ShouldNotBeNull();
+        result.EndTime.ShouldNotBeNull();
+        result.State.ShouldBe(HospitalizationState.FINISHED);
+    
+    }
+}
