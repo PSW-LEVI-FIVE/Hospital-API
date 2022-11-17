89d29799a9a6a43618cef977399b1ed680514ba2
Dusan-Stanimirovic <dussanstanimirovic@gmail.com>
2022-11-17 18:48:00 +0100

    made innitial EquipmentReallocation

---
+++ src/HospitalLibrary/Rooms/Model/EquipmentReallocation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary.Rooms.Model
+{
+    public class EquipmentReallocation
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity), Key()]
+        public int Id { get; set; }
+
+        [ForeignKey("Room")]
+        public int RoomId { get; set; }
+
+        public Room Room { get; set; }
+
+        public DateTime StartAt { get; set; }
+        public DateTime EndAt { get; set; }
+
+        public EquipmentReallocation() 
+        {
+        }
+    }
+}
---
+++ src/HospitalLibrary/Rooms/Repositories/EquipmentReallocationRepository.cs
@@ -0,0 +1,31 @@
+﻿using HospitalLibrary.Appointments;
+using HospitalLibrary.Rooms.Interfaces;
+using HospitalLibrary.Rooms.Model;
+using HospitalLibrary.Settings;
+using HospitalLibrary.Shared.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary.Rooms.Repositories
+{
+    public class EquipmentReallocationRepository : BaseRepository<EquipmentReallocation>, IEquipmentReallocationRepository
+    {
+        public EquipmentReallocationRepository(HospitalDbContext dataContext) : base(dataContext)
+        {
+        }
+
+        public async Task<IEnumerable<TimeInterval>> GetAllRoomTakenInrevalsForDate(int roomId, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EquipmentReallocation> GetById(int appointmentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
