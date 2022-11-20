204b3cf81fcbd2ec3d6456bf57e166652e66ec1a
Dušan Janošević <dusan.janosevic123@gmail.com>
2022-11-20 17:43:25 +0100

    refactored EmailService

--- src/HospitalAPI/Emails/EmailService.cs
+++ src/HospitalAPI/Emails/EmailService.cs
@@ -8,90 +8,66 @@ namespace HospitalAPI.Emails
 {
     public class EmailService : IEmailService
     {
-        public async Task SendAppointmentEmail(string email)
+        private static MailMessage GenerateMailMessage(string email)
         {
             MailMessage newMail = new MailMessage();
             newMail.From = new MailAddress("levi.five.hospital@gmail.com", "Levi Five");
             newMail.To.Add(email);
-            newMail.Subject = "Appointment assigned!";
-            newMail.Body = "<h2>Your appointment has been successfully added!<h2/><br/>" +
-                            "<h2><strong>Sent from LEVI-FIVE Hospital Service!</strong></h2>";
+            return newMail;
+        }
+        private static void SendMail(MailMessage newMail)
+        {
             newMail.IsBodyHtml = true;
-            
             SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
             client.EnableSsl = true;
             NetworkCredential netCre = new NetworkCredential("levi.five.hospital@gmail.com", "fnaafrzlyeaxqupx");
             client.Credentials = netCre;
             client.Send(newMail);
+        }
+        public async Task SendAppointmentEmail(string email)
+        {
+            MailMessage newMail = GenerateMailMessage(email);
+            newMail.Subject = "Appointment assigned!";
+            newMail.Body = "<h2>Your appointment has been successfully added!<h2/><br/>" +
+                            "<h2><strong>Sent from LEVI-FIVE Hospital Service!</strong></h2>";
+            SendMail(newMail);
         }
 
         public async Task SendWelcomeEmail(string email)
         {
-            MailMessage newMail = new MailMessage();
-            newMail.From = new MailAddress("levi.five.hospital@gmail.com", "Levi Five");
-            newMail.To.Add(email);
+            MailMessage newMail = GenerateMailMessage(email);
             newMail.Subject = "Welcome!";
             newMail.Body = "<h2>Welcome to LEVI-FIVE Hospital Service!<h2/><br/>" +
                            "<h2><strong>Sent from LEVI-FIVE Hospital Service!</strong></h2>";
-            newMail.IsBodyHtml = true;
-            
-            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
-            client.EnableSsl = true;
-            NetworkCredential netCre = new NetworkCredential("levi.five.hospital@gmail.com", "fnaafrzlyeaxqupx");
-            client.Credentials = netCre;
-            client.Send(newMail);
+            SendMail(newMail);
         }
 
         public async Task SendWelcomeEmailWithActivationLink(string email)
         {
-            MailMessage newMail = new MailMessage();
+            MailMessage newMail = GenerateMailMessage(email);
             newMail.Subject = "Welcome to LeviFive hospital!";
-            newMail.From = new MailAddress("levi.five.hospital@gmail.com", "Levi Five");
-            newMail.To.Add(email);
             newMail.Body = "<h1>Welcome to LEVI-FIVE Hospital Service!</h1> <br/>" +
                            "<h2><a href=\"https://www.youtube.com/watch?v=dQw4w9WgXcQ\">Here is your Prize!<a/></h2> <br/>" +
                            "<h2><strong>Sent from LEVI-FIVE Hospital Service!</strong></h2>";
-            newMail.IsBodyHtml = true;
-            
-            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
-            client.EnableSsl = true;
-            NetworkCredential netCre = new NetworkCredential("levi.five.hospital@gmail.com", "fnaafrzlyeaxqupx");
-            client.Credentials = netCre;
-            client.Send(newMail);
+            SendMail(newMail);
         }
 
         public async Task SendAppointmentCanceledEmail(string email, DateTime time)
         {
-            MailMessage newMail = new MailMessage();
+            MailMessage newMail = GenerateMailMessage(email);
             newMail.Subject = "Canceled appointment!";
-            newMail.From = new MailAddress("levi.five.hospital@gmail.com", "Levi Five");
-            newMail.To.Add(email);
             newMail.Body = "<h1>Your appointment for " + time +"has been canceled</h1> <br/>" +
                            "<h2><strong>Sent from LEVI-FIVE Hospital Service!</strong></h2>";
-            newMail.IsBodyHtml = true;
-            
-            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
-            client.EnableSsl = true;
-            NetworkCredential netCre = new NetworkCredential("levi.five.hospital@gmail.com", "fnaafrzlyeaxqupx");
-            client.Credentials = netCre;
-            client.Send(newMail);
+            SendMail(newMail);
         }
 
         public async Task SendAppointmentRescheduledEmail(string email, DateTime time, DateTime newTime)
         {
-            MailMessage newMail = new MailMessage();
+            MailMessage newMail = GenerateMailMessage(email);
             newMail.Subject = "Rescheduled appointment!";
-            newMail.From = new MailAddress("levi.five.hospital@gmail.com", "Levi Five");
-            newMail.To.Add(email);
             newMail.Body = "<h1>Your appointment for " + time +"has been moved to"+ newTime +"</h1> <br/>" +
                            "<h2><strong>Sent from LEVI-FIVE Hospital Service!</strong></h2>";
-            newMail.IsBodyHtml = true;
-            
-            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
-            client.EnableSsl = true;
-            NetworkCredential netCre = new NetworkCredential("levi.five.hospital@gmail.com", "fnaafrzlyeaxqupx");
-            client.Credentials = netCre;
-            client.Send(newMail);
+            SendMail(newMail);
         }
     }
 }
