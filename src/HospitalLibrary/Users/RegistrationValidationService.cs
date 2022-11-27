7ae81c801b31dce66b6464f0fcb8161218a273ca
Dušan Janošević <dusan.janosevic123@gmail.com>
2022-11-27 19:05:28 +0100

    added real time unique validations

--- src/HospitalLibrary/Users/RegistrationValidationService.cs
+++ src/HospitalLibrary/Users/RegistrationValidationService.cs
@@ -6,7 +6,7 @@ using HospitalLibrary.Users.Interfaces;
 
 namespace HospitalLibrary.Users
 {
-    public class RegistrationValidationService:IRegistrationValidationService
+    public class RegistrationValidationService : IRegistrationValidationService
     {
         private IUnitOfWork _unitOfWork;
 
@@ -17,12 +17,30 @@ namespace HospitalLibrary.Users
         
         public async Task ValidatePatientRegistration(User user)
         {
-            if (_unitOfWork.PersonRepository.GetOneByEmail(user.Person.Email) != null)
-                throw new BadRequestException("Email is already taken"); 
-            if(_unitOfWork.PersonRepository.GetOneByUid(user.Person.Uid) != null)
-                throw new BadRequestException("Uid is already taken");
-            if(_unitOfWork.UserRepository.GetOneByUsername(user.Username) != null)
+            await IsUsernameUnique(user.Username);
+            await IsEmailUnique(user.Person.Email);
+            await IsUidUnique(user.Person.Uid);
+        }
+
+        public async Task<bool> IsUsernameUnique(string username)
+        {
+            if(_unitOfWork.UserRepository.GetOneByUsername(username) != null)
                 throw new BadRequestException("Username is already taken");
+            return true;
+        }
+
+        public async Task<bool> IsEmailUnique(string email)
+        {
+            if(_unitOfWork.PersonRepository.GetOneByEmail(email) != null)
+                throw new BadRequestException("Email is already taken");
+            return true;
+        }
+
+        public async Task<bool> IsUidUnique(string uid)
+        {
+            if(_unitOfWork.PersonRepository.GetOneByUid(uid) != null)
+                throw new BadRequestException("Uid is already taken");
+            return true;
         }
     }
 }
