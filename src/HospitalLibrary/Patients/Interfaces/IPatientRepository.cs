fb21d713d01c55cb65b9690f371c45461e6bfb42
Kristian Krizzan <krizan.ra196.2019@uns.ac.rs>
2022-12-12 17:44:21 +0100

    rebasing 2/3

--- src/HospitalLibrary/Patients/Interfaces/IPatientRepository.cs
+++ src/HospitalLibrary/Patients/Interfaces/IPatientRepository.cs
@@ -6,7 +6,11 @@ namespace HospitalLibrary.Patients.Interfaces
 {
     public interface IPatientRepository: IBaseRepository<Patient>
     {
+<<<<<<< HEAD
         public Patient SearchByUid(string uid);
         public Task<IEnumerable<Patient>> GetMaliciousPatients();
+=======
+        Task<IEnumerable<Patient>> GetMaliciousPatients();
+>>>>>>> 8f11d8c (getting poetential malicious users)
     }
 }
