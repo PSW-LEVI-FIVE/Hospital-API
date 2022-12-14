0cee5cca89df231db6f72b2427558a4fb2d19e19
mihajlo182savic <savic.ra182.2019@uns.ac.rs>
2022-12-14 22:22:37 +0100

    Validation added

--- src/HospitalLibrary/Shared/Model/ValueObjects/PhoneNumber.cs
+++ src/HospitalLibrary/Shared/Model/ValueObjects/PhoneNumber.cs
@@ -12,25 +12,27 @@ namespace HospitalLibrary.Shared.Model.ValueObjects
         public PhoneNumber(string cellNumber)
         {
             CellNumber = cellNumber;
+            Validate();
         }
         
         public PhoneNumber(PhoneNumber phoneNumber)
         {
             CellNumber = phoneNumber.CellNumber;
+            Validate();
         }
         
         private void Validate()
         {
-            if (CellNumber.Trim().Equals(""))
+            Regex validatePhoneNumberRegex = new Regex("^\\+?[1-9][0-9]{7,14}$");
+            if(!validatePhoneNumberRegex.IsMatch(CellNumber))
             {
-                throw new Exception("All address fields should be filled!");
+                throw new Exception("Invalid phone number");
             }
         }
         
         protected override bool EqualsCore(PhoneNumber other)
         {
-            Regex validatePhoneNumberRegex = new Regex("^\\+?[1-9][0-9]{7,14}$");
-            return validatePhoneNumberRegex.IsMatch(other.CellNumber);;
+            return CellNumber.Equals(other.CellNumber);
         }
         
         protected override int GetHashCodeCore()
