0f243e085c00804b83db284c6415c2eb02bce920
mihajlo182savic <savic.ra182.2019@uns.ac.rs>
2022-12-14 23:32:35 +0100

    VO for phone number added

---
+++ src/HospitalLibrary/Migrations/20221214215606_VO_PhoneNumber.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HospitalLibrary.Migrations
+{
+    public partial class VO_PhoneNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumber",
+                table: "Persons",
+                newName: "PhoneNumber_CellNumber");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumber_CellNumber",
+                table: "Persons",
+                newName: "PhoneNumber");
+        }
+    }
+}
