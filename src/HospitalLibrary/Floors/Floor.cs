3f50e93126ca31482b0e7e756b87ec8afc81aa5f
mihajlo182savic <savic.ra182.2019@uns.ac.rs>
2022-12-15 17:07:42 +0100

    entity

--- src/HospitalLibrary/Floors/Floor.cs
+++ src/HospitalLibrary/Floors/Floor.cs
@@ -4,22 +4,40 @@ using System.ComponentModel.DataAnnotations.Schema;
 using HospitalLibrary.Buildings;
 using HospitalLibrary.Rooms;
 using HospitalLibrary.Rooms.Model;
+using HospitalLibrary.Shared.Model;
 
 namespace HospitalLibrary.Floors
 {
-    public class Floor
+    public class Floor : BaseEntity
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity), Key()]
-        public int Id { get; set; }
-        public int Number { get; set; }
-        public float Area { get; set; }
+        public int Id { get;private set; }
+        public int Number { get;private set; }
+        public float Area { get;private set; }
         
-        public virtual ICollection<Room> Rooms { get; set; }
+        public virtual ICollection<Room> Rooms { get;private set; }
         
         [ForeignKey("Building")]
-        public int BuildingId { get; set; }
-        public Building Building { get; set; }
+        public int BuildingId { get;private set; }
+        public Building Building { get;private set; }
+
+        public Floor(int number, float area)
+        {
+            Number = number;
+            Area = area;
+        }
+
+        public Floor(int id, float area, int buildingId)
+        {
+            Id = id;
+            Area = area;
+            BuildingId = buildingId;
+        }
+        public Floor(int buildingId)
+        {
+            BuildingId = buildingId;
+        }
         
-        public Floor() {}
+        
     }
 }
