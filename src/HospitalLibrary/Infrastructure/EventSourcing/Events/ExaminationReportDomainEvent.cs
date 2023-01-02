d2e02b0b7a04b0f1c84a7523fdeb72b98a948108
srdjan1210 <businessrdjan@gmail.com>
2023-01-02 16:24:46 +0100

    Added domain event for examination report

---
+++ src/HospitalLibrary/Infrastructure/EventSourcing/Events/ExaminationReportDomainEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HospitalLibrary.Infrastructure.EventSourcing.Events
+{
+    public enum ExaminationReportEventType { STARTED, ADDED_SYMPTOM, ADDED_REPORT, ADDED_PRESCRIPTION, FINISHED }
+    
+    public class ExaminationReportDomainEvent: DomainEvent
+    {
+        public ExaminationReportEventType Type { get; private set; }
+        
+        public ExaminationReportDomainEvent(int aggregateId, DateTime timestamp, string name, ExaminationReportEventType type) : base(aggregateId, timestamp, name)
+        {
+            Type = type;
+        }
+    }
+}
