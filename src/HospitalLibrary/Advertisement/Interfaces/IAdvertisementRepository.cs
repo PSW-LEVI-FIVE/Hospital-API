74de8b3238f8975772a235a54ba1c2851c287ff2
mihajlo182savic <savic.ra182.2019@uns.ac.rs>
2023-01-04 20:23:08 +0100

    Advertisement model

---
+++ src/HospitalLibrary/Advertisement/Interfaces/IAdvertisementRepository.cs
@@ -0,0 +1,10 @@
+﻿using HospitalLibrary.Allergens;
+using HospitalLibrary.Shared.Interfaces;
+
+namespace HospitalLibrary.Advertisement.Interfaces
+{
+    public interface IAdvertisementRepository : IBaseRepository<Advertisement>
+    {
+        
+    }
+}
