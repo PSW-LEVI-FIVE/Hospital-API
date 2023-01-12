7e2c6f3e4e5054568bcf7ce37bd79bff6757a991
DavidT <terenji.ra178.2019@uns.ac.rs>
2023-01-12 21:25:23 +0100

    blood orders fixed

--- src/HospitalAPI/Controllers/Intranet/BloodOrderController.cs
+++ src/HospitalAPI/Controllers/Intranet/BloodOrderController.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using HospitalLibrary.BloodOrders;
 using HospitalLibrary.BloodOrders.Dtos;
 using HospitalLibrary.BloodOrders.Interfaces;
+using HospitalLibrary.Users;
+using HospitalLibrary.Users.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,6 +27,7 @@ namespace HospitalAPI.Controllers.Intranet
         [Authorize(Roles="Doctor")]
         public async Task<IActionResult> Create([FromBody] CreateBloodOrderDto bloodOrderDto)
         {
+            bloodOrderDto.DoctorId = GetCurrentUser().Id;
             BloodOrder bloodOrder = await _bloodOrderService.Create(bloodOrderDto.MapToModel());
             return Ok(bloodOrder);
         }
@@ -32,6 +37,24 @@ namespace HospitalAPI.Controllers.Intranet
         {
             List<ShowBloodOrderDto> bloodOrders = await _bloodOrderService.GetAllBloodOrders();
             return Ok(bloodOrders);
+        }
+        
+        private UserDTO GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                return new UserDTO
+                {
+                    Id = int.Parse(userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value),
+                    Role = Role.Doctor,
+                    Username = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Name)?.Value
+                };
+            }
+
+            return null;
         }
     }
 }
